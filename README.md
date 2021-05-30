4290cdc93f0f5360b9759f2123ef8c8606cd0f81
easycastle <sean34418290@gmail.com>
2021-05-30 10:55:50 +0900

    feat : add path option

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Youtube-Video-Downloader
+
+(youtube-dl options)[https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312]
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 from tkinter import *
+from tkinter import filedialog
 
 window = Tk()
 window.title('youtube_video_downloader')
@@ -8,8 +11,18 @@ window.resizable(False, False)
 def addVideo():
     pass
 
+def browsePath():
+    folder_selected = filedialog.askdirectory()
+    
+    if folder_selected == '':
+        return
+
+    pathEntry.delete(0, END)
+    pathEntry.insert(0, folder_selected)
+
 def extraction():
     pass
+
 
 linkFrmae = LabelFrame(window, text='링크')
 linkFrmae.pack(fill='x', padx=5, pady=5, ipady=5)
@@ -18,6 +31,7 @@ linkEntry = Entry(linkFrmae).pack(side='left', fill='x', expand=True, padx=5, pa
 
 addVideoBtn = Button(linkFrmae, text='링크 추가', width=10, command=addVideo)
 addVideoBtn.pack(side='right', padx=5, pady=5)
+
 
 listFrame = Frame(window)
 listFrame.pack(fill='both', padx=5, pady=5)
@@ -29,6 +43,17 @@ linkList = Listbox(listFrame, selectmode='extended', height=10, yscrollcommand=s
 linkList.pack(side='left', fill='both', expand=True)
 scrollbar.config(command=linkList.yview)
 
+
+pathFrame = LabelFrame(window, text='저장경로')
+pathFrame.pack(fill='x', padx=5, pady=5, ipady=5)
+
+pathEntry = Entry(pathFrame)
+pathEntry.pack(side='left', fill='x', expand=True, padx=5, pady=5, ipady=4)
+
+pathBtn = Button(pathFrame, text='찾아보기', width=10, command=browsePath)
+pathBtn.pack(side='right', padx=5, pady=5)
+
+
 formatFrame = LabelFrame(window, text='파일 형식')
 formatFrame.pack(side='left', padx=5, pady=5)
 
@@ -39,7 +64,14 @@ MP3_Rad.select()
 MP3_Rad.pack(side='left')
 MP4_Rad.pack(side='left')
 
-extractionBtn = Button(window, text='추출', width=12, padx=5, pady=5, command=extraction)
+
+runFrame = Frame(window)
+runFrame.pack(side='right')
+
+startBtn = Button(runFrame, text='닫기', width=12, padx=5, pady=5, command=window.quit)
+startBtn.pack(side='right', padx=5, pady=5)
+
+extractionBtn = Button(runFrame, text='추출', width=12, padx=5, pady=5, command=extraction)
 extractionBtn.pack(side='right', padx=5, pady=5)
 
 window.mainloop()
